9c7923c62df5003505b6066e88654eb8ceb3f6e6
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-01-14 10:44:28 -0600

    Added props and styled Catch of the Day

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -3,7 +3,19 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1>Header</h1>
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline">
+          <span>{this.props.tagline}</span>
+        </h3>
+      </header>
     )
   }
 
