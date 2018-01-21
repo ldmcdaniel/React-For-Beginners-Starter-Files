90280afdad6667a6d9471dd07e7fc395416f251d
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-01-20 21:31:54 -0600

    Added state for adding new fishes

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <h1>Inventory</h1>
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish}/>
+      </div>
     )
   }
 }
