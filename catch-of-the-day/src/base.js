52c4c662bec301a5be4333db110440d8eb4b81c9
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-01-26 21:45:57 -0600

    Added firebase state to App.js

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDxMbMy2xv53MXbymfyBlctB7XX5yMBYl4",
+  authDomain: "catch-of-the-day-ecd63.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-ecd63.firebaseio.com",
+});
+
+export default base;
