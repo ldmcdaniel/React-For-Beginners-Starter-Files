eaca9c73cf65cf7df9c37808ffba467411f9b44c
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-02-04 15:38:39 -0600

    Added auth to the app

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -108,6 +108,7 @@ class App extends React.Component {
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
           updateFish={this.updateFish}
+          storeId={this.props.params.storeId}
         />
       </div>
     )
