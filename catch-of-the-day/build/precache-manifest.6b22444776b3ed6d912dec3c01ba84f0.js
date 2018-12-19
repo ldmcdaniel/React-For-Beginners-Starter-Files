9a92900968a927042df32402478521f6c9a232da
Lucas McDaniel <lmcdaniel@myemma.com>
2018-12-19 13:32:39 -0600

    Added build files

---
+++ catch-of-the-day/build/precache-manifest.6b22444776b3ed6d912dec3c01ba84f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/fish-market/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "775ff34324cce18791eb",
+    "url": "/fish-market/static/css/main.2c554430.chunk.css"
+  },
+  {
+    "revision": "b667883aeddf6fa09c22",
+    "url": "/fish-market/static/js/1.b667883a.chunk.js"
+  },
+  {
+    "revision": "3c6e9ca85f39e3caf841",
+    "url": "/fish-market/static/js/runtime~main.3c6e9ca8.js"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/fish-market/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/fish-market/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/fish-market/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "775ff34324cce18791eb",
+    "url": "/fish-market/static/js/main.775ff343.chunk.js"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/fish-market/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/fish-market/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/fish-market/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/fish-market/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/fish-market/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "917143d7b2a657bd7f0cb4778b874c0f",
+    "url": "/fish-market/index.html"
+  }
+];
